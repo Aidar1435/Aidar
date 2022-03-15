4588578a77de5bcc68ecca9b03b305542d0b472f
Aidar1435 <101663549+Aidar1435@users.noreply.github.com>
2022-03-16 00:11:06 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,28 +1,3 @@
 sudo /etc/init.d/mysql start
 mysql -uroot -e "create database Aidar;"
-~/web/ask/manage.py makemigrations
-~/web/ask/manage.py migrate
 
-DATABASES = {
-
-    'default': {
-
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Aidar',
-        'USER': 'box',
-    }
-}
-
-from django.contrib.auth.models import User
-
-class QuestionManager(models.Manager):
-  def new(self):
-    return self.order_by('-added_at')
-  def popular(self):
-    return self.order_by('-rating')
-    
-    added_at = models.DateTimeField(auto_now_add=True)
-    
-    rating = models.IntegerField(default=0)
-    
-    likes = models.ManyToManyField(User, related_name='question_like_user')
